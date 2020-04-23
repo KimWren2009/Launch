0d010b59eec92fe873f7c3f52a90b3415cb1604f
KimWren2009 <kimwren2009@gmail.com>
2020-04-23 08:50:29 -0400

    adding exercises from final chapter

---
+++ RB100-Programming_Prep/9-Final_Exercises/10.rb
@@ -0,0 +1,6 @@
+# Can hash values be arrays? 
+# Can you have an array of hashes? (give examples)
+
+pets = { furry: ['dog', 'cat', 'hampster']} 
+
+pets = [ {dog: 'bark'}, {cat: 'meow'}, {hampster: 'squeek'}]
---
+++ RB100-Programming_Prep/9-Final_Exercises/8.rb
@@ -0,0 +1,6 @@
+# Create a Hash, with one key-value pair, 
+# using both Ruby syntax styles
+
+numbers = { :one => 1 }
+
+numbers_alt = { one: 1 }
