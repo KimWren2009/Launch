a7ba42c776938c3b349e71c7c0913627ba09b8d2
KimWren2009 <kimwren2009@gmail.com>
2020-04-24 13:31:30 -0400

    adding exercises from RB100 final review

---
+++ RB100-Programming_Prep/RB100-Exercises/array_5.rb
@@ -0,0 +1,6 @@
+# Using the code below, select 'dog' from pets, 
+# Use Array#each to iterate over colors and print each element.
+
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each { |color| puts "The color is #{color}!" } 
