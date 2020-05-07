3d146e966cdafced424627dc3fac9e859e9b7969
KimWren2009 <kimwren2009@gmail.com>
2020-05-07 10:18:00 -0400

    updating mortgage calculator

--- RB101/lesson_02/16_mortgage_calculator/mortgage_calculator.rb
+++ RB101/lesson_02/16_mortgage_calculator/mortgage_calculator.rb
@@ -5,54 +5,71 @@ def prompt(message)
   puts("=> #{message}")
 end
 
-def valid_loan?(amount)
-  amount.to_i.to_s == amount
+def retrieve_input(input_type)
+  input = nil
+  loop do
+    input = gets.chomp
+    break if valid_input?(input, input_type)
+    invalid_input_message(input_type)
+  end
+  system('clear')
+  input
 end
 
-def valid_apr?(percentage)
-  percentage.to_f.to_s == percentage || percentage.to_i.to_s == percentage
+def valid_input?(input, input_type)
+  case input_type
+  when 'loan_amount'
+    input.to_i.to_s == input && input.to_i > 0
+  when 'annual_interest'
+    input.to_i.to_s == input && input.to_i > 0 ||
+      input.to_f.to_s == input && input.to_i > 0
+  when 'duration_years'
+    input.to_i.to_s == input && valid_duration?(input)
+  end
 end
+
+def valid_duration?(input)
+  duration_options = %w(10 15 20 25 30 40)
+  duration_options.include?(input)
+end
+
+def invalid_input_message(input_type)
+  case input_type
+  when 'loan_amount'
+    prompt(MESSAGES['invalid_loan_amount'])
+  when 'annual_interest'
+    prompt(MESSAGES['invalid_apr'])
+  when 'duration_years'
+    prompt(MESSAGES['invalid_duration'])
+  end
+end
+
+system('clear')
 
 prompt(MESSAGES['welcome'])
 
-loan_amount = nil
-annual_interest = nil
-years = nil
-years_options = %w(10 15 20 25 30 40)
-
 loop do
   prompt(MESSAGES['input_loan_amount'])
-  loop do
-    loan_amount = gets.chomp
-    break if valid_loan?(loan_amount)
-    prompt(MESSAGES['invalid_loan_amount'])
-  end
+  loan_amount = retrieve_input('loan_amount')
 
   puts "Great! The loan amount is #{loan_amount}. What is the APR (%)?"
-  loop do
-    annual_interest = gets.chomp
-    break if valid_apr?(annual_interest)
-    prompt(MESSAGES['invalid_apr'])
-  end
+  annual_interest = retrieve_input('annual_interest')
 
-  puts 'What is the duration of your mortgage (in years)?'
-  loop do
-    years = gets.chomp
-    break if years.to_i.to_s == years && years_options.include?(years)
-    prompt(MESSAGES['invalid_duration'])
-  end
+  puts "APR is #{annual_interest}. What is your mortgage duration (in years)?"
+  duration_years = retrieve_input('duration_years')
 
   monthly_interest = ((annual_interest.to_f / 100) / 12)
-  months = (years.to_i * 12)
+  duration_months = (duration_years.to_i * 12)
   monthly_payment = loan_amount.to_i *
                     (monthly_interest.to_f /
-                    (1 - (1 + monthly_interest.to_f)**-months))
+                    (1 - (1 + monthly_interest.to_f)**-duration_months))
 
   puts "Your monthly payment is: $#{format('%02.2f', monthly_payment)}."
 
-  prompt(MESSAGES['another_loan'])
+  prompt(MESSAGES['calculate_again'])
   answer = gets.chomp.downcase
-  break unless answer.start_with?('y')
+  break unless answer == 'y' || answer == 'yes'
+  system('clear')
 end
 
 prompt(MESSAGES['goodbye'])
